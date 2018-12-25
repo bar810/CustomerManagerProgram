b87caba87bcba745b66dbba2c38d3bb44fdafe6d
bar810 <bar810@gmail.com>
2018-12-25 20:11:34 +0200

    infrastructure 3 - Architecture completely

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import entities.Subscription;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,12 +7,8 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import utils.GlobalProperties;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static utils.Constants.MAIN_PROGRAM_TEXT;
 import static utils.GlobalProperties._logger;
-import static utils.SQLQueries.SQLQueriesAgainstSubscription.*;
 
 public class Main extends Application {
 
@@ -45,22 +40,6 @@ public class Main extends Application {
         _logger.debug("Program Started");
         //launch(args);
 
-        Subscription a= new Subscription(1,"meal",1.5,Subscription.SubscriptionType.MEALS);
-        Subscription a1=new Subscription(1,"vip",1.5,Subscription.SubscriptionType.VIP);
-        List<Subscription> l=new ArrayList<>();
-        l.add(a);
-        l.add(a1);
-        insertRangeOfSubscriptionsToDB(l);
-
-        Subscription s=getSubscriptionByID(1);
-
-        List<Subscription> ll=getAllSubscriptionFromDBWithConditions("","","meal","1","0");
-
-        updateSubscriptionBalance(1,50);
-
-        removeOneSubscription(2);
-
-        removeAllSubscriptionsTable();
 
         _logger.debug("Program Finished");
         GlobalProperties.closeConnections();
--- src/utils/CSVScripts/CSVScripts.java
+++ src/utils/CSVScripts/CSVScripts.java
@@ -8,6 +8,7 @@ package utils.CSVScripts;
 
 import entities.Customer;
 import entities.Purchase;
+import entities.Subscription;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -22,7 +23,7 @@ import static utils.GlobalProperties._logger;
  */
 public class CSVScripts {
 
-    public static void exportPurchasesToCsv(ArrayList<Purchase> list) {
+    public static void exportPurchasesToCsv(List<Purchase> list) {
         try
         {
             _logger.debug("Exporting to csv");
@@ -64,7 +65,7 @@ public class CSVScripts {
         }
         _logger.debug("Exporting to csv finished successfully");
     }
-    public static ArrayList<Purchase> importPurchasesFromCsv(String location) {
+    public static List<Purchase> importPurchasesFromCsv(String location) {
         _logger.debug("Import Purchases from CSV. location: "+location);
         List<String[]> content = new ArrayList<>();
         try(BufferedReader br = new BufferedReader(new FileReader(location))) {
@@ -91,10 +92,7 @@ public class CSVScripts {
         _logger.debug("Purchases imported successfully. size:  "+purchases.size());
         return purchases;
     }
-
-
-
-    public static void exportCustomersToCsv(ArrayList<Customer> list) {
+    public static void exportCustomersToCsv(List<Customer> list) {
         try
         {
             _logger.debug("Exporting to csv");
@@ -136,7 +134,7 @@ public class CSVScripts {
         }
         _logger.debug("Exporting to csv finished successfully");
     }
-    public static ArrayList<Customer> importCustomersFromCsv(String location) {
+    public static List<Customer> importCustomersFromCsv(String location) {
         _logger.debug("Import Customers from CSV. location: "+location);
         List<String[]> content = new ArrayList<>();
         try(BufferedReader br = new BufferedReader(new FileReader(location))) {
@@ -155,5 +153,75 @@ public class CSVScripts {
         }
         _logger.debug("Customers imported successfully. size:  "+customers.size());
         return customers;
+    }
+    public static void exportSubscriptionsToCsv(List<Subscription> list) {
+        try
+        {
+            _logger.debug("Exporting to csv");
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("Subscriptions.csv"), "UTF-8"));
+            //write the header
+            StringBuffer oneLine = new StringBuffer();
+            oneLine.append("ID");
+            oneLine.append(CSV_SEPERATOR);
+            oneLine.append("Customer ID");
+            oneLine.append(CSV_SEPERATOR);
+            oneLine.append("Purchase date");
+            oneLine.append(CSV_SEPERATOR);
+            oneLine.append("Balance");
+            oneLine.append(CSV_SEPERATOR);
+            oneLine.append("Type");
+            bw.write(oneLine.toString());
+            bw.newLine();
+
+            for (Subscription subscription : list)
+            {
+                oneLine = new StringBuffer();
+                oneLine.append(subscription.getSubscriptionID() <=0 ? "" : subscription.getSubscriptionID());
+                oneLine.append(CSV_SEPERATOR);
+                oneLine.append(subscription.getCoustomerID()< 0? "" : subscription.getCoustomerID());
+                oneLine.append(CSV_SEPERATOR);
+                oneLine.append(subscription.getSubscriptionPurchaseDate().trim().length() == 0? "" : subscription.getSubscriptionPurchaseDate());
+                oneLine.append(CSV_SEPERATOR);
+                oneLine.append(subscription.getBalance()< 0? "" : subscription.getBalance());
+                oneLine.append(CSV_SEPERATOR);
+                oneLine.append(subscription.getType().getValue().trim().length() <0? "" : subscription.getType().getValue());
+                bw.write(oneLine.toString());
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+        }
+        catch (Exception e) {
+            _logger.error("Error when trying to export to CSV. "+e.toString());
+        }
+        _logger.debug("Exporting to csv finished successfully");
+    }
+    public static List<Subscription> importSubscriptionsFromCsv(String location) {
+        _logger.debug("Import Subscriptions from CSV. location: "+location);
+        List<String[]> content = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(location))) {
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                content.add(line.split(","));
+            }
+        } catch (Exception e) {
+            _logger.error("Error while trying to import from csv. "+e.toString());
+        }
+        ArrayList<Subscription> subscriptions=new ArrayList<>();
+        //return content;
+        for(int i=1;i<content.size();i++){
+            String[] con=content.get(i);
+            try {
+                int customerID= Integer.parseInt(con[1]);
+                String date=con[2];
+                double balance= Double.parseDouble(con[3]);
+                Subscription.SubscriptionType type= Subscription.SubscriptionType.valueOf(con[4]);
+                subscriptions.add(new Subscription(customerID,date,balance,type));
+            } catch (Exception ex) {
+                _logger.debug("Subscriptions reading failed. probably cannot cast string to int");
+            }
+        }
+        _logger.debug("Subscriptions imported successfully. size:  "+subscriptions.size());
+        return subscriptions;
     }
 }
